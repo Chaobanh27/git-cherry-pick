3af76f84e6ae8b5363b4542ce079abb9e77376ab
ChaoBanhNguyen <102232031+Chaobanh27@users.noreply.github.com>
2024-09-30 22:05:24 +0700

    task 26 (#2)

--- task-23.js
+++ task-23.js
@@ -5,3 +5,4 @@
 // Trungquandev test conflict (from task-25)
 // Trungquandev test conflict (from task 24)
 // Nguyen Chanh Bao test conflict (from task 27)
+// nguyen chanh bao test conflict (from task 26)
